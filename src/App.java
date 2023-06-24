40b3a065d7a9394c7c806a54ad6f20fc1d2a1581
Iago <iago.luiz@ges.inatel.br>
2023-06-24 20:22:18 -0300

    :construction: feat: implemented minefield

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import campo.model.Tabuleiro;
+import campo.view.TabuleiroConsole;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Tabuleiro tabuleiro = new Tabuleiro(6, 6, 3);
+        new TabuleiroConsole(tabuleiro);
     }
 }
